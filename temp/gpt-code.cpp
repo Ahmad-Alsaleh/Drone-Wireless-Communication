fc2445ecd06144a37b9a6a127a9f6c53841b45aa
Ahmad Alsaleh <ahmed.asaleh2@gmail.com>
2025-04-14 04:06:12 +0400

    write `transmitImageChunks`

--- temp/gpt-code.cpp
+++ temp/gpt-code.cpp
@@ -16,8 +16,8 @@
 // TODO: replace with real image
 #define IMAGE_WIDTH 3
 #define IMAGE_HEIGHT 2
-#define N_IMAGE_BYTES 6
-const u8 image_data[N_IMAGE_BYTES] = {0, 1, 2, 3, 4, 5};
+#define N_IMAGE_BYTES 764
+const u8 image_data[N_IMAGE_BYTES] = {FAKE_IMAGE};
 
 #define CHUNK_SIZE 200
 
@@ -46,32 +46,63 @@ void sendPacket(const u8 *data, usize len) {
   // integration testing with the server code.
 }
 
-// sends `LORA<N_BYTES_TO_SEND><IMAGE_WIDTH><IMAGE_HEIGHT>`
+// sends `LORA<N_BYTES_TO_SEND><IMAGE_WIDTH><IMAGE_HEIGHT>` where
+// LORA is 4 bytes
+// <N_BYTES_TO_SEND> is 4 bytes
+// <IMAGE_WIDTH> is 4 bytes
+// <IMAGE_HEIGHT> is 4 bytes
 void transmitHeader(u32 n_bytes_to_send) {
   debug(">[DEBUG] transmiting header...\n");
 
   u8 header[16];
   void *cursor = header;
 
-  // write `LORA` to buffer
+  // write "LORA" literal to buffer
   const char *prefix = "LORA";
   cursor = mempcpy(cursor, prefix, strlen(prefix));
 
-  // write `n_bytes_to_send` to buffer
+  // write <N_BYTES_TO_SEND> to buffer
   n_bytes_to_send = htonl(n_bytes_to_send);
   cursor = mempcpy(cursor, &n_bytes_to_send, sizeof(n_bytes_to_send));
 
-  // write `image_width` to buffer
+  // write <IMAGE_WIDTH> to buffer
   u32 image_width = htonl(IMAGE_WIDTH);
   cursor = mempcpy(cursor, &image_width, sizeof(image_width));
 
-  // write `image_height` to buffer
+  // write <IMAGE_HEIGHT> to buffer
   u32 image_height = htonl(IMAGE_HEIGHT);
   cursor = mempcpy(cursor, &image_height, sizeof(image_height));
 
   sendPacket(header, 16);
 
   debug("<[DEBUG] transmiting header\n");
+}
+
+// sends `<CHUNK_SEQ_NUM><CHUNK_BYTES>`
+void transmitImageChunks(u16 n_chunks) {
+  debug(">[DEBUG] transmiting image chunks...\n");
+
+  u8 chunk[CHUNK_SIZE + 2];
+  void *cursor;
+  for (u16 chunk_i = 0; chunk_i < n_chunks; ++chunk_i) {
+    cursor = chunk;
+
+    // write <CHUNK_SEQ_NUM> to buffer
+    u16 chunk_seq_num = htons(chunk_i);
+    cursor = mempcpy(chunk, &chunk_seq_num, sizeof(chunk_seq_num));
+
+    // write <CHUNK_BYTES> to buffer
+    usize start = chunk_i * CHUNK_SIZE;
+    // last chunk might be slightly shorter than CHUNK_SIZE
+    // N_IMAGE_BYTES - start is the lenght of the remaining chunks
+    // it can be less than CHUNK_SIZE only if it's the last chunk
+    usize chunk_len = min(CHUNK_SIZE, N_IMAGE_BYTES - start);
+    cursor = mempcpy(cursor, image_data + start, chunk_len);
+
+    sendPacket(chunk, chunk_len + 2);
+  }
+
+  debug("<[DEBUG] done transmiting image chunks\n");
 }
 
 void transmitImage() {
@@ -84,6 +115,7 @@ void transmitImage() {
   u32 n_bytes_to_send = N_IMAGE_BYTES + 2 * n_chunks;
 
   transmitHeader(n_bytes_to_send);
+  transmitImageChunks(n_chunks);
 }
 
 void setup() {
