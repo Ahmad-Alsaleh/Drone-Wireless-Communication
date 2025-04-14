099aa37b8a88bf60ee9c66e850c3684e8e0080e2
Ahmad Alsaleh <ahmed.asaleh2@gmail.com>
2025-04-15 02:45:34 +0400

    replace old main.cpp with new one that is better and implements missed chunks retransmission

--- main.cpp
+++ main.cpp
@@ -1,134 +1,234 @@
+// #define ARDUINO
 #define DEBUG
-
-#include "utils.h" // TODO: remove this
 
 #ifdef DEBUG
 #define debug(...) fprintf(stderr, __VA_ARGS__)
-#include <cstdio>
-#include <cstring>
-#include <stdint.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/_endian.h>
-
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-
-class SerialClass {
-public:
-  void begin(unsigned long baud) { debug("Starting Serial (%ld)\n", baud); }
-  void write(const u8 *bytes, size_t length) {
-    for (size_t i = 0; i < length - 1; ++i)
-      printf("%02x ", bytes[i]);
-    printf("%02x\n", bytes[length - 1]);
-  }
-  void print(const char *msg) { printf("%s", msg); }
-  void println(const char *msg) { printf("%s\n", msg); }
-} Serial;
-
 #else
-#include <Arduino.h> // TODO: remove this
 #define debug(...)
-#endif
+#endif // DEBUG
 
-const u32 CHUNK_SIZE = 200; // no need for u32, maybe use #define
-const u32 IMAGE_WIDTH = 10;
-const u32 IMAGE_HEIGHT = 7;
+#ifdef ARDUINO
+#include <Arduino.h> // TODO: try to remove this
+#else
+#include "utils.h"
+#endif // ARDUINO ---
 
-// TODO: Adham sends the header as a prefix to the 1st chunk.
-// However, I send it as a separate chunk to simplyfy the code.
-// To fix this, make a function called `transmit_first_chunk` and
-// put it inside `transmit_image_chunks`
-void transmit_header(u32 n_bytes_to_send, u32 image_width, u32 image_height) {
-  debug("[DEBUG] transmiting header...\n");
-  u8 buffer[36];
-  size_t buffer_i = 0;
+// TODO: replace with real image
+#define IMAGE_WIDTH 3
+#define IMAGE_HEIGHT 2
+#define N_IMAGE_BYTES 764
+const u8 image_data[N_IMAGE_BYTES] = {FAKE_IMAGE};
 
-  const char *prefix = "AT+TEST=TXLRPKT, \"LORA";
-  memcpy(buffer + buffer_i, prefix, strlen(prefix));
-  buffer_i += strlen(prefix);
+#define CHUNK_SIZE 200
+#define RETRANSMISSION_TIMEOUT 5000
+#define RX_SWITCH_DELAY 500
 
-  n_bytes_to_send = htonl(n_bytes_to_send);
-  memcpy(buffer + buffer_i, &n_bytes_to_send, sizeof(n_bytes_to_send));
-  buffer_i += sizeof(n_bytes_to_send);
+void configLora() {
+  debug(">[DEBUG] sending lora configs...\n");
 
-  image_width = htonl(image_width);
-  memcpy(buffer + buffer_i, &image_width, sizeof(image_width));
-  buffer_i += sizeof(image_width);
+  // TODO: read after every write to discard confirmation msg
+  Serial.write("AT+LOG=QUIET\n", 13);
+  Serial.write("AT+UART=BR, 230400\n", 19);
+  Serial.write("AT+MODE=TEST\n", 13);
+  Serial.write("AT+TEST=RFCFG,868,SF7,250,12,15,14,ON,OFF,OFF\n", 46);
 
-  image_height = htonl(image_height);
-  memcpy(buffer + buffer_i, &image_height, sizeof(image_height));
-  buffer_i += sizeof(image_height);
+  debug("<[DEBUG] done sending lora configs\n");
+}
 
-  const char *suffex = "\"\n";
-  memcpy(buffer + buffer_i, suffex, strlen(suffex));
-  buffer_i += strlen(suffex);
+// discards bytes in the serial buffer until a terminator.
+// Note that this function uses `Serial.read()` internally
+// which doesn't respect the Serial timeout value, the function
+// will block while the Serial buffer is empty.
+void discardSerialBytesUnitl(char terminator) {
+  while (!Serial.available() || Serial.read() != terminator)
+    ;
+}
 
-  Serial.write(buffer, buffer_i);
+// discards a specific number of bytes from the serial buffer.
+// Note that this function uses `Serial.read()` internally
+// which doesn't respect the Serial timeout value, the function
+// will block while the Serial buffer is empty.
+void discardNSerialBytes(usize length) {
+  while (length--) {
+    // block until the buffer has some bytes to read
+    while (!Serial.available())
+      ;
+    Serial.read();
+  }
+}
 
-  // NOTE: Adham transmitts the body as hex and UTF-8 encodes the whole buffer
+// writes `AT+TEST=TXLRPKT, "<DATA>"\n` to serial as bytes
+void sendPacket(const u8 *data, usize len) {
+  Serial.write("AT+TEST=TXLRPKT, \"", 18);
+  Serial.write(data, len);
+  Serial.write("\"\n", 2);
+
+  // discard confirmation message recieved from AT commands (two lines)
+  discardSerialBytesUnitl('\n');
+  discardSerialBytesUnitl('\n');
+
+  // NOTE: Adham transmits the body as hex and UTF-8 encodes the whole buffer
   // before sending it. I don't think i need to do this. but it is better to do
   // integration testing with the server code.
 }
 
-void transmit_image_chunk(u16 chunk_seq_num, u8 image_bytes[], u32 chunk_len) {
-  debug("[DEBUG] transmiting image chunk #%d (%d bytes)...\n", chunk_seq_num,
-        chunk_len);
+// sends `LORA<N_BYTES_TO_SEND><IMAGE_WIDTH><IMAGE_HEIGHT>` where
+// LORA is 4 bytes
+// <N_BYTES_TO_SEND> is 4 bytes
+// <IMAGE_WIDTH> is 4 bytes
+// <IMAGE_HEIGHT> is 4 bytes
+void transmitHeader(u32 n_bytes_to_send) {
+  // TODO: Adham is sending the header 3 times in a row, do i need to do this?
 
-  u8 buffer[22 + CHUNK_SIZE];
-  size_t buffer_i = 0;
+  debug(">[DEBUG] transmitting header...\n");
 
-  const char *prefix = "AT+TEST=TXLRPKT, \"";
-  memcpy(buffer + buffer_i, prefix, strlen(prefix));
-  buffer_i += strlen(prefix);
+  u8 header[16];
+  void *cursor = header;
 
-  u16 seq_num_big_endian = htons(chunk_seq_num);
-  memcpy(buffer + buffer_i, &seq_num_big_endian, sizeof(chunk_seq_num));
-  buffer_i += sizeof(chunk_seq_num);
+  // write "LORA" literal to buffer
+  cursor = mempcpy(cursor, "LORA", 4);
 
-  memcpy(buffer + buffer_i, image_bytes + chunk_seq_num * CHUNK_SIZE,
-         chunk_len);
-  buffer_i += chunk_len;
+  // write <N_BYTES_TO_SEND> to buffer
+  n_bytes_to_send = htonl(n_bytes_to_send);
+  cursor = mempcpy(cursor, &n_bytes_to_send, sizeof(n_bytes_to_send));
 
-  const char *suffex = "\"\n";
-  memcpy(buffer + buffer_i, suffex, strlen(suffex));
-  buffer_i += strlen(suffex);
+  // write <IMAGE_WIDTH> to buffer
+  u32 image_width = htonl(IMAGE_WIDTH);
+  cursor = mempcpy(cursor, &image_width, sizeof(image_width));
 
-  Serial.write(buffer, buffer_i);
+  // write <IMAGE_HEIGHT> to buffer
+  u32 image_height = htonl(IMAGE_HEIGHT);
+  cursor = mempcpy(cursor, &image_height, sizeof(image_height));
+
+  sendPacket(header, 16);
+
+  debug("<[DEBUG] transmitting header\n");
 }
 
-void transmit_image_chunks(u8 image_bytes[], u16 n_chunks) {
-  // transmit all chunks except the last one since it might have fewer bytes
-  // than the other chunks
-  for (u16 chunk_i = 0; chunk_i < n_chunks - 1; ++chunk_i)
-    transmit_image_chunk(chunk_i, image_bytes, CHUNK_SIZE);
-  // transmit the last chunk
-  transmit_image_chunk(n_chunks - 1, image_bytes,
-                       n_image_bytes - (n_chunks - 1) * CHUNK_SIZE);
+// transmits a single chunk in the following format:
+// `<CHUNK_SEQ_NUM><CHUNK_BYTES>` where <CHUNK_SEQ_NUM> is 2 bytes
+// and <CHUNK_BYTES> is `CHUNK_SIZE` bytes (or maybe less for the last chunk)
+void transmitSingleImageChunk(u16 chunk_seq_num) {
+  debug(">[DEBUG] transmitting image chunk with seq #%hu...\n", chunk_seq_num);
+
+  u8 chunk[CHUNK_SIZE + 2];
+  void *cursor = chunk;
+
+  // the chunk start index in the image's array of bytes
+  usize chunk_start_i = chunk_seq_num * CHUNK_SIZE;
+
+  // write <CHUNK_SEQ_NUM> to buffer
+  chunk_seq_num = htons(chunk_seq_num);
+  cursor = mempcpy(cursor, &chunk_seq_num, sizeof(chunk_seq_num));
+
+  // write <CHUNK_BYTES> to the buffer.
+  // the last chunk might be slightly shorter than `CHUNK_SIZE`,
+  // so `min` is used.
+  usize chunk_len = min(CHUNK_SIZE, N_IMAGE_BYTES - chunk_start_i);
+  cursor = mempcpy(cursor, image_data + chunk_start_i, chunk_len);
+
+  sendPacket(chunk, chunk_len + 2);
+
+  debug("<[DEBUG] done transmitting image chunk\n");
 }
 
-#ifdef DEBUG
-int main() {
-#else
-void loop() {}
-void setup() {
-#endif
-  Serial.begin(115200);
+// transmits the image as chunks
+void transmitImageChunks(u16 n_chunks) {
+  debug(">[DEBUG] transmitting image chunks (%hu chunk/s)...\n", n_chunks);
 
+  for (u16 chunk_i = 0; chunk_i < n_chunks; ++chunk_i)
+    transmitSingleImageChunk(chunk_i);
+
+  debug("<[DEBUG] done transmitting image chunks\n");
+}
+
+// reads from Serial the sequence numbers of missed chunks
+// and retransmits the bytes of the missed chunks
+void retransmitMissedChunks() {
+  Serial.setTimeout(RETRANSMISSION_TIMEOUT);
+
+  // enable recieve mode to recieve sequence numbers of missed chunks
+  Serial.write("AT+TEST=RXLRPKT\n", 16);
+  // discard confirmation message recieved from AT commands
+  discardSerialBytesUnitl('\n');
+
+  // the AT message recieved has the form:
+  // +TEST: LEN:<LEN>, RSSI:<RSSI>, SNR:<SNR>\r\n
+  // +TEST: RX "<PAYLOAD>"\r\n
+
+  // we are interested in the payload after the first `"` in the second line.
+  // discard the first line and the bytes before the <PAYLOAD>.
+  discardSerialBytesUnitl('\n');
+  discardSerialBytesUnitl('"');
+
+  // the payload recieved has the form:
+  // `MISS<N_MISSED_CHUNKS><SEQ_1><SEQ_2>...<SEQ_N>`
+  // where <N_MISSED_CHUNKS> and <SEQ_i> are 2 bytes each.
+
+  // discard the four bytes of the "MISS" literal
+  discardNSerialBytes(4);
+
+  // read and parse the two bytes of <N_MISSED_CHUNKS>.
+  // note that ther protocol uses big endian.
+  u16 n_missed_chunks = (Serial.read() << 8) | Serial.read();
+
+  debug("[DEBUG] Number of missed chunks: %hu", n_missed_chunks);
+
+  // TODO: instead of malloc, we can use a static array of
+  // size, say, 256 bytes here since according to @Yousef an
+  // image is at most 15KB which is 15000 / 200 = 75 chunks.
+  // Thus 256 bytes should more than enough
+
+  // the recieved payload has `n_missed_chunks` sequence numbers
+  // and each sequence number is 2 bytes.
+  u8 *buffer = (u8 *)malloc(2 * n_missed_chunks);
+
+  // read the sequence numbers of the missed chunks
+  Serial.readBytes(buffer, 2 * n_missed_chunks);
+
+  // TODO: understand why we need this delay, and whether it should be inside
+  // the loop
+
+  // wating before sending missed chunks
+  delay(RX_SWITCH_DELAY);
+
+  // retransmit the missed chunks
+  for (u16 missed_chunk_i = 0; missed_chunk_i < n_missed_chunks;
+       ++missed_chunk_i) {
+
+    u16 chunk_seq_num =
+        (buffer[2 * missed_chunk_i] << 8) | buffer[2 * missed_chunk_i + 1];
+
+    debug("[DEBUG] Transmitting missed chunk #%hu/%hu: Seq #%hu\n",
+          missed_chunk_i, n_missed_chunks, chunk_seq_num);
+
+    transmitSingleImageChunk(chunk_seq_num);
+  }
+
+  // switch Serial timeout back to default value
+  Serial.setTimeout(1000);
+}
+
+void transmitImage() {
   // manually implement the ceil function
-  u16 n_chunks = n_image_bytes / CHUNK_SIZE;
-  if (n_image_bytes != n_chunks * CHUNK_SIZE) {
+  u16 n_chunks = N_IMAGE_BYTES / CHUNK_SIZE;
+  if (N_IMAGE_BYTES != n_chunks * CHUNK_SIZE) {
     ++n_chunks;
   }
 
-  u32 n_bytes_to_send = n_image_bytes + 2 * n_chunks;
+  u32 n_bytes_to_send = N_IMAGE_BYTES + 2 * n_chunks;
 
-  // TODO: Adham send the header 3 times. Should I do that as well?
-  transmit_header(n_bytes_to_send, IMAGE_WIDTH, IMAGE_HEIGHT);
-  transmit_image_chunks(image_bytes, n_chunks);
+  transmitHeader(n_bytes_to_send);
+  transmitImageChunks(n_chunks);
+  retransmitMissedChunks();
+}
 
-#ifdef DEBUG
-  return 0;
-#endif
+void setup() {
+  Serial.begin(115200); // TODO: this might need to be 230400
+  configLora();
+  transmitImage();
+}
+
+void loop() {
+  // nothing to do here
 }
